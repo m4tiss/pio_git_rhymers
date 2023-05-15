8dd1963902606646cb2f7eda3b7f4a25261ae327
m4tiss <mateusz039@op.pl>
2023-05-15 14:51:48 +0200

    17.Stworzenie dokumentacji funkcji klasy HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,10 +5,18 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
     public static final int ZERO_REJECTED = 0;
     private int totalRejected = ZERO_REJECTED;
 
+    /**
+     *
+     * @return total number of rejected Rhymers
+     */
     public int reportRejected() {
         return totalRejected;
     }
 
+    /**
+     *
+     * @param in line 23 increments total number of rejected Rhymers
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
@@ -17,6 +25,10 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             super.countIn(in);
     }
 
+    /**
+     *
+     * @return total number of rejected Rhymers
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
